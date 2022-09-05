ad5e3855e43c4b7566eedbc72816776724aabc75
avishkakavindu <avishkakavindud@gmail.com>
2022-09-05 10:22:24 +0530

    DB config completed

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
 const { Client } = require('pg');
+const pool = require('./db');
 
 dotenv.config();
 
@@ -12,6 +13,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const res = await app.listen(PORT, () => {
+// Routes
+
+app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}...`);
 });
