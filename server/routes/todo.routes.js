1f473ddf9682f5b6ee3836b9e8b0b4593d43d3f1
avishkakavindu <avishkakavindud@gmail.com>
2022-09-05 12:43:32 +0530

    CRUD routes completed

--- server/routes/todo.routes.js
+++ server/routes/todo.routes.js
@@ -17,7 +17,60 @@ module.exports = (params) => {
     }
   });
 
- 
+  // get all todo
+  router.route('/').get(async (req, res) => {
+    try {
+      const todos = await pool.query('SELECT * FROM todo');
+
+      return res.json(todos.rows);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  // get todo by id
+  router.route('/:id').get(async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const todo = await pool.query('SELECT * FROM todo WHERE todo_id=$1', [
+        id,
+      ]);
+
+      return res.json(todo.rows);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  // update todo
+  router.route('/:id').put(async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { description } = req.body;
+
+      const todo = await pool.query(
+        'UPDATE todo SET description=$1 WHERE todo_id=$2 returning *',
+        [description, id]
+      );
+
+      return res.json(todo.rows);
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  // delete todo
+  router.route('/:id').delete(async (req, res) => {
+    try {
+      const { id } = req.params;
+
+      const todo = await pool.query('DELETE FROM todo WHERE todo_id=$1', [id]);
+
+      return res.json(`Todo id: ${id} is deleted!`);
+    } catch (err) {
+      console.log(err);
+    }
   });
 
   return router;
