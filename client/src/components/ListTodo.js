873f03f81ef643e30d66b781d4e35175c4e19784
avishkakavindu <avishkakavindud@gmail.com>
2022-09-05 14:57:43 +0530

    Delete todo completed

--- client/src/components/ListTodo.js
+++ client/src/components/ListTodo.js
@@ -1,32 +1,61 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useEffect, useState } from 'react';
 
 const ListTodo = () => {
+  const [todos, setTodos] = useState([]);
+
+  // delete func
+  const deleteTodo = async (id) => {
+    try {
+      const todo = await fetch(`http://127.0.0.1:5000/todos/${id}`, {
+        method: 'DELETE',
+      });
+      setTodos(todos.filter((todo) => todo.todo_id !== id));
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const getTodos = async () => {
+    try {
+      const res = await fetch('http://127.0.0.1:5000/todos');
+      const jsonData = await res.json();
+
+      setTodos(jsonData);
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+
+  useEffect(() => {
+    getTodos();
+  }, []);
   return (
     <Fragment>
       <table className="table">
         <thead>
           <tr>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>Email</th>
+            <th>Description</th>
+            <th>Edit</th>
+            <th>Delete</th>
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>John</td>
-            <td>Doe</td>
-            <td>john@example.com</td>
-          </tr>
-          <tr>
-            <td>Mary</td>
-            <td>Moe</td>
-            <td>mary@example.com</td>
-          </tr>
-          <tr>
-            <td>July</td>
-            <td>Dooley</td>
-            <td>july@example.com</td>
-          </tr>
+          {todos.map((todo) => {
+            return (
+              <tr key={todo.todo_id}>
+                <td>{todo.description}</td>
+                <td>Edit</td>
+                <td>
+                  <button
+                    className="btn btn-danger"
+                    onClick={() => deleteTodo(todo.todo_id)}
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            );
+          })}
         </tbody>
       </table>
     </Fragment>
