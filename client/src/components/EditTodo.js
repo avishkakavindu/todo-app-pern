a227ed693bc86188883bfef00b1c1c627c06cdb8
avishkakavindu <avishkakavindud@gmail.com>
2022-09-05 16:44:08 +0530

    Edit todo completed

---
+++ client/src/components/EditTodo.js
@@ -0,0 +1,84 @@
+import React, { Fragment, useState } from 'react';
+
+const EditTodo = (todo) => {
+  const [description, setDescription] = useState(todo.todo.description);
+
+  // updateTodo func
+  const updateTodo = async (id) => {
+    try {
+      const body = { description };
+
+      const updatedTodo = await fetch(
+        `http://127.0.0.1:5000/todos/${todo.todo.todo_id}`,
+        {
+          method: 'PUT',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(body),
+        }
+      );
+
+      window.location.reload();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Fragment>
+      <button
+        type="button"
+        className="btn btn-warning"
+        data-bs-toggle="modal"
+        data-bs-target={`#id${todo.todo.todo_id}`}
+      >
+        Edit
+      </button>
+
+      <div className="modal" id={`id${todo.todo.todo_id}`}>
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Edit Todo</h4>
+              <button
+                type="button"
+                className="btn-close"
+                data-bs-dismiss="modal"
+                onClick={() => setDescription(todo.todo.description)}
+              ></button>
+            </div>
+
+            <div className="modal-body">
+              <input
+                type="text"
+                className="form-control"
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+              />
+            </div>
+
+            <div className="modal-footer">
+              <button
+                type="button"
+                className="btn btn-warning"
+                data-bs-dismiss="modal"
+                onClick={(e) => updateTodo(e)}
+              >
+                Edit
+              </button>
+              <button
+                type="button"
+                className="btn btn-danger"
+                data-bs-dismiss="modal"
+                onClick={() => setDescription(todo.todo.description)}
+              >
+                Close
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default EditTodo;
